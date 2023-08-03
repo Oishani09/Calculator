034412bc458071a49638b3b9ae1544935647cf07
Oishani Banerjee <oishanibanerjee09@gmail.com>
2023-08-03 13:28:44 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,13 +1,9 @@
 function clearScreen() {
     document.getElementById("result").value = "";
 }
- 
-// This function display values
 function display(value) {
     document.getElementById("result").value += value;
 }
- 
-// This function evaluates the expression and returns result
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
